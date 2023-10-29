e34d54ccfacb9930f4077bf2553726a5074ac80e
Brendo Lopez <brendoslopes@gmail.com>
2023-10-29 18:11:19 -0300

    Adicionar arquivos de projeto.

---
+++ ProjectWorker/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectWorker.Entities
+{
+    public class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+
+}
---
+++ ProjectWorker/Entities/Worker.cs
@@ -0,0 +1,15 @@
+﻿using ProjectWorker.Entities.Enums;
+
+
+namespace ProjectWorker.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+
+
+    }
+}
